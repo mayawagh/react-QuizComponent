515ffc496390fcec8439e2e9f54609bf79ebbd20
mayawagh <mayawagh1@gmail.com>
2020-07-07 14:41:59 +0100

    Add files via upload

---
+++ src/QuizQuestion.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
+
+class QuizQuestion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
+
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
+      this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
+    }
+  }
+  render() {
+    return (
+      <main>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right.</p>
+        ) : (
+          ""
+        )}
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.props.quiz_question.answer_options.map((item, index) => (
+              <QuizQuestionButton
+                key={index}
+                button_text={item}
+                clickHandler={this.handleClick.bind(this)}
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default QuizQuestion;
